e1394f0cb7f68f64c80b2038c4a3fb6c795ef11e
Ahmed <ahmedreda152@gmail.com>
2022-01-04 21:34:49 +0200

    add the reviewer required changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
+import React from 'react';
 import './App.css';
-import SignButton from './components/Buttons/BasicSigns';
-import NumberButton from './components/Buttons/NumberButton';
-import CalcInput from './components/CalcInput';
-import { numbers, signs } from './static/button';
+import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="w-2/4 items-center">
-      <div className="w-full">
-        <CalcInput />
-      </div>
-      <div className="flex">
-        <div className="grid  grid-cols-3  w-4/5 self-start">
-          {signs.secondary.map((sign) => (
-            <SignButton key={sign.id} sign={sign.name} type="sec" />
-          ))}
-          {numbers.map((number) => (
-            <NumberButton key={number.id} number={number.name} />
-          ))}
-        </div>
-        <div className="flex flex-col w-1/5">
-          {signs.basic.map((sign) => (
-            <SignButton key={sign.id} sign={sign.name} type="basic" />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return <Calculator />;
+  }
 }
 
 export default App;
