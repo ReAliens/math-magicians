47d40a960aadb52a6a94a8403871aa8af76ca2ea
Ahmed <ahmedreda152@gmail.com>
2022-01-04 20:24:37 +0200

    add UI structure and tailwind css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import SignButton from './components/Buttons/BasicSigns';
+import NumberButton from './components/Buttons/NumberButton';
+import CalcInput from './components/CalcInput';
+import { numbers, signs } from './static/button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-2/4 items-center">
+      <div className="w-full">
+        <CalcInput />
+      </div>
+      <div className="flex">
+        <div className="grid  grid-cols-3  w-4/5 self-start">
+          {signs.secondary.map((sign) => (
+            <SignButton key={sign.id} sign={sign.name} type="sec" />
+          ))}
+          {numbers.map((number) => (
+            <NumberButton key={number.id} number={number.name} />
+          ))}
+        </div>
+        <div className="flex flex-col w-1/5">
+          {signs.basic.map((sign) => (
+            <SignButton key={sign.id} sign={sign.name} type="basic" />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/static/button.js
@@ -0,0 +1,104 @@
+export const numbers = [
+  {
+    id: '7',
+    name: '7',
+    value: 7,
+  },
+  {
+    id: '8',
+    name: '8',
+    value: 8,
+  },
+  {
+    id: '9',
+    name: '9',
+    value: 9,
+  },
+  {
+    id: '4',
+    name: '4',
+    value: 4,
+  },
+  {
+    id: '5',
+    name: '5',
+    value: 5,
+  },
+  {
+    id: '6',
+    name: '6',
+    value: 6,
+  },
+  {
+    id: '1',
+    name: '1',
+    value: 1,
+  },
+  {
+    id: '2',
+    name: '2',
+    value: 2,
+  },
+  {
+    id: '3',
+    name: '3',
+    value: 3,
+  },
+  {
+    id: '0',
+    name: '0',
+    value: 0,
+  },
+  {
+    id: 'dot-sign',
+    name: '.',
+    value: '.',
+  },
+];
+
+export const signs = {
+  basic: [
+    {
+      id: 'add',
+      name: '+',
+      action: 'add',
+    },
+    {
+      id: 'substract',
+      name: '-',
+      action: 'substract',
+    },
+    {
+      id: 'multiply',
+      name: '*',
+      action: 'multiply',
+    },
+    {
+      id: 'divide',
+      name: '/',
+      action: 'divide',
+    },
+    {
+      id: 'equal',
+      name: '=',
+      action: 'equal',
+    },
+  ],
+  secondary: [
+    {
+      id: 'reset',
+      name: 'AC',
+      action: 'reset',
+    },
+    {
+      id: 'abs',
+      name: '+/-',
+      action: 'abs',
+    },
+    {
+      id: 'percentage',
+      name: '%',
+      action: 'percentage',
+    },
+  ],
+};
